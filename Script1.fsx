b8ab7c68e64b1cac63e64a299305f1659c033823
Max Wilson <wilson.max@gmail.com>
2021-01-31 12:26:21 -0800

    Add outer shell of parsing

--- Script1.fsx
+++ Script1.fsx
@@ -1,7 +1,17 @@
 ﻿#I __SOURCE_DIRECTORY__
 #load "Parsing.fs"
+open Packrat
 
 type Param = Param of name: string * type': string * attributes: string list
-type AST =
+type ProgramFragment =
     | Namespaces of string list
     | Function of name: string * args: Param list
+
+let (|Program|_|) = function
+    | Any(any, rest) ->
+        Some ([Namespaces([any])], rest)
+    | _ -> None
+
+let parse = parser (|Program|_|)
+
+parse "System.Text"
