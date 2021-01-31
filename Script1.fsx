f95c7133ea8989a25c6717451d03a49ee96dbc0b
Max Wilson <wilson.max@gmail.com>
2021-01-31 12:20:09 -0800

    Initial project

---
+++ Script1.fsx
@@ -0,0 +1,7 @@
+﻿#I __SOURCE_DIRECTORY__
+#load "Parsing.fs"
+
+type Param = Param of name: string * type': string * attributes: string list
+type AST =
+    | Namespaces of string list
+    | Function of name: string * args: Param list
